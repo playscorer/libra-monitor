b43e0f1256cb6fb33dbff306304acaa49bbff8b7
Filipe Abalada <filipe.abalada@gmail.com>
2018-05-15 02:37:30 +0100

    Monitor simplest version ever

--- src/main/java/arbitrail/libramonitor/LibraMonitorController.java
+++ src/main/java/arbitrail/libramonitor/LibraMonitorController.java
@@ -1,5 +1,8 @@
 package arbitrail.libramonitor;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,24 +11,25 @@ import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
+import java.util.stream.Collectors;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LibraMonitorController {
-	
+
 	private final static Logger LOG = Logger.getLogger(LibraMonitorController.class);
-	
-	private String destLogFile = "src/main/resources/static/libra.html";
-	
+
 	@Value("${logdirpath}")
 	private String LOGDIRPATH;
-	
-	public void refreshLogFile() {
+
+/*	private Path getLogFile() {
 		try {
 			WatchService watchService = FileSystems.getDefault().newWatchService();
 			Path rootPath = Paths.get(LOGDIRPATH);
@@ -36,21 +40,28 @@ public class LibraMonitorController {
 				for (WatchEvent<?> event : key.pollEvents()) {
 					Path logFilePath = (Path) event.context();
 					LOG.debug("Event kind:" + event.kind() + ". File changed : " + logFilePath + ".");
-					Path logFileAbsolutePath = rootPath.resolve(logFilePath);
-					Path destLogFilePath = Paths.get(destLogFile);
-					Files.copy(logFileAbsolutePath, destLogFilePath);
+					return rootPath.resolve(logFilePath);
 				}
 				key.reset();
 			}
 		} catch (Exception e) {
 			LOG.error("Unexpected Exception :", e);
 		}
+		return null;
+	}*/
+
+	@ResponseBody
+	@RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+	public String index() {
+		//Path logPath = getLogFile();
+		Path logPath = Paths.get(LOGDIRPATH).resolve("libra.html");
+		Charset charset = Charset.forName("UTF-8");
+		try (BufferedReader reader = Files.newBufferedReader(logPath, charset)) {
+			return reader.lines().collect(Collectors.joining("\n"));
+		} catch (IOException ioe) {
+		    LOG.error("IOException : ", ioe);
+		}
+		return "<meta http-equiv=\"refresh\" content=\"" + 30 + "\"><h1>No Log file</h1>";
 	}
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
-		refreshLogFile();
-		return "redirect:/libra.html";
-    }
-	
 }
